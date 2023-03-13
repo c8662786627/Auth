d1b6ad2a4a1e1c7e10ffea2dd08bbdac3f9ab684
leon <c890132422@gmail.com>
2023-03-13 10:52:11 +0800

    註冊功能完成

--- app/Http/Controllers/UserAuthController.php
+++ app/Http/Controllers/UserAuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
+use App\Models\User;
 
 class UserAuthController extends Controller
 {
@@ -105,10 +106,33 @@ class UserAuthController extends Controller
 
        $validator = Validator::make($input,$rules);
        if($validator->fails()){
-      // return redirect('/user/auth/sign-up')->withErrors($vaildator);
-        return response($validator->errors());
+       return redirect('/user/auth/sign-up')->withErrors($validator)->withInput();
+        //return response($validator->errors());
        }
+
+       $email = request()->input('email');
+       //檢查email使否存在
+       $user = User::where('email', $email)->first();
+
+       if ($user) {
+           return redirect('/user/auth/sign-up')->withErrors(['email' => '該信箱已被註冊過了！'])->withInput();
+       } else {
+            //創建用戶
+            $user = new User;
+            $user->name = request()->input('nickname');
+            $user->email = request()->input('email');
+            $user->password = bcrypt(request()->input('password'));
+            $user->type = request()->input('type');
+            $user->save();
+       }
+
+      
+
        return response()->json(true);
+       //return redirect('login')->with('success', '註冊成功請重新登入！');
+    }
+    public function signInProcess(){
+
     }
     public function getCSRFToken(){
         return csrf_token();
--- resources/views/auth/signUp.blade.php
+++ resources/views/auth/signUp.blade.php
@@ -10,19 +10,19 @@
     <form action="/user/auth/sign-up" method="post">
         <label for="">
             暱稱:
-            <input type="text" name="nickname" placeholder="暱稱">
+            <input type="text" name="nickname" placeholder="暱稱" value="{{old('nickname')}}">
         </label>
         <label for="">
             Email:
-            <input type="text" name="email" placeholder="暱稱">
+            <input type="text" name="email" placeholder="Email" value="{{old('email')}}" >
         </label>
         <label for="">
             密碼:
-            <input type="password" name="password" placeholder="暱稱">
+            <input type="password" name="password" placeholder="密碼" value="{{old('password')}}">
         </label>
         <label for="">
             確認密碼:
-            <input type="password" name="password_confirmation" placeholder="暱稱">
+            <input type="password" name="password_confirmation" placeholder="確認密碼" value="{{old('password_confirmation')}}">
         </label>
         <label for="">
             帳號類型:
@@ -31,6 +31,15 @@
                 <option value="A">管理者</option>
             </select>
         </label>
+        @if ($errors->any())
+            <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+            </div>
+        @endif  
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
         <button type="submit">註冊</button>
     </form>
